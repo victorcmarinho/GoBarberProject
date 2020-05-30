59c69e1f06e83bba59cf990707569fc4e50aa6bf
Victor Marinho <victorcmarinho@gmail.com>
2020-05-30 20:56:16 -0300

    Finalizando listagem de agendamento

--- gobarberweb/src/hooks/AuthContext.tsx
+++ gobarberweb/src/hooks/AuthContext.tsx
@@ -16,6 +16,7 @@ interface AuthUserData {
     name: string;
     email: string;
     avatar: string | null;
+    avatar_url: string;
 }
 
 interface IAuthContext {
@@ -31,16 +32,25 @@ export const AuthProvider: React.FC = ({ children }) => {
     const [data, setData] = useState<AuthState>(() => {
         const token = localStorage.getItem('@Gobarber:token');
         const user = localStorage.getItem('@Gobarber:user');
-        return token && user ? {token, user: JSON.parse(user)} : {} as AuthState;
+        
+        if(token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            return {token, user: JSON.parse(user)}
+        }
+        return {} as AuthState;
+        
     });
 
 
 
     const signIn = useCallback(async ({email, password}) => {
         const response = await api.post('sessions', {email, password});
-        const {token, user } = response.data;
+        const { token, user } = response.data;
         localStorage.setItem('@Gobarber:token', token);
         localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+        api.defaults.headers.authorization = `Bearer ${token}`;
+
         setData({token, user});
     }, []);
 
