1432067eaa5e450b496747214a36cce085651bc9
Victor Marinho <victorcmarinho@gmail.com>
2020-06-24 11:47:04 -0300

    init unit test app

---
+++ appgobarber/src/setupTests.ts
@@ -0,0 +1,4 @@
+//@ts-ignore
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
